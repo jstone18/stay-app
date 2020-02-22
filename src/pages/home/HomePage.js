43106c740cba03d52b8423b8c4729b4ce86610a7
jstone18 <jason.stone0808@gmail.com>
2020-02-22 00:12:31 -0500

    Add axios, Create async api call for cities

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import "./homePage.css";
+import axios from "axios";
 import SearchBox from "../../components/search-box/SearchBox";
+import Spinner from "../../components/spinner/Spinner";
 
-const HomePage = () => {
-	return (
-		<div className="container-fluid">
-			<div className="row">
-				<div className="home col s12">
-					<div className="upper-fold">
-						<SearchBox />
+export default class HomePage extends Component {
+	state = {
+		cities: []
+	};
+
+	async componentDidMount() {
+		const citiesUrl = `${window.apiHost}/cities/recommended`;
+		const recommendedCities = await axios.get(citiesUrl);
+		this.setState({ cities: recommendedCities.data });
+	}
+	render() {
+		if (this.state.cities.length === 0) {
+			return <Spinner />;
+		}
+		return (
+			<div className="container-fluid">
+				<div className="row">
+					<div className="home col s12">
+						<div className="upper-fold">
+							<SearchBox />
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	);
-};
-
-export default HomePage;
+		);
+	}
+}
