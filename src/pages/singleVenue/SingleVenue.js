0574890eb8374c715964d4ced7b963bf516f69af
jstone18 <jason.stone0808@gmail.com>
2020-02-24 23:28:35 -0500

    Update singleVenue page with reservation form

--- src/pages/singleVenue/SingleVenue.js
+++ src/pages/singleVenue/SingleVenue.js
@@ -4,7 +4,13 @@ import "./singleVenue.css";
 import Point from "./Point";
 
 class SingleVenue extends Component {
-	state = { singleVenue: {}, points: [] };
+	state = {
+		singleVenue: {},
+		points: [],
+		numberOfGuests: 0,
+		checkIn: "",
+		checkOut: ""
+	};
 
 	async componentDidMount() {
 		const venueId = this.props.match.params.venueId;
@@ -23,6 +29,22 @@ class SingleVenue extends Component {
 		this.setState({ singleVenue, points });
 	}
 
+	changeNumberOfGuests = e => {
+		this.setState({ numberOfGuests: e.target.value });
+	};
+	changeCheckIn = e => {
+		this.setState({ checkIn: e.target.value });
+	};
+	changeCheckOut = e => {
+		this.setState({ checkOut: e.target.value });
+	};
+
+	reserveNow = e => {
+		alert("Reservation submitted!");
+
+		this.setState({ numberOfGuests: 0, checkIn: "", checkOut: "" });
+	};
+
 	render() {
 		const {
 			id,
@@ -40,13 +62,53 @@ class SingleVenue extends Component {
 				<div className="col s12 center">
 					<img src={imageUrl} alt="venue" />
 				</div>
+
+				{/* Location details */}
 				<div className="col s8 location-details offset-s2">
+					{/* left side */}
 					<div className="col s8 left-details">
 						<div className="location">{location}</div>
 						<div className="title">{title}</div>
 						<div className="guests">Guests: {guests}</div>
-						<div className="divider"></div>
+
+						<div className="divider" />
 						{this.state.points}
+
+						<div className="details">{details}</div>
+						<div className="amenities">{amenities}</div>
+					</div>
+
+					{/* right side */}
+					<div className="col s4 right-details">
+						<div className="price-per-day">
+							${pricePerNight} <span>per day</span>
+						</div>
+						<div className="rating">{rating}</div>
+						<div className="col s6">
+							Check-In
+							<input type="date" />
+						</div>
+						<div className="col s6">
+							Check-Out
+							<input type="date" />
+						</div>
+						<div className="col s12">
+							<select className="browser-default">
+								<option value="1">1 Guest</option>
+								<option value="2">2 Guest</option>
+								<option value="3">3 Guest</option>
+								<option value="4">4 Guest</option>
+								<option value="5">5 Guest</option>
+								<option value="6">6 Guest</option>
+								<option value="7">7 Guest</option>
+								<option value="8">8 Guest</option>
+							</select>
+						</div>
+						<div className="col s12 center">
+							<button className="btn red accent-2" onClick={this.reserveNow}>
+								Reserve
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
